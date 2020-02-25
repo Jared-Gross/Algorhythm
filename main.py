e87dfb2aedaafb97fa7e503f83f557ea11c02808
JareBear <jaredgrozz@gmail.com>
2020-02-25 11:06:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ class mainwindowUI(QMainWindow):
         self.actionPrefrences.setStatusTip('Change window settings.')
         self.actionPrefrences.triggered.connect(self.open_settings_window)
 
-        self.actionOpenMusicPlayer = self.findChild(QAction, 'actionOpen_Music_Player')
+        self.actionOpenMusicPlayer = self.findChild(QAction, 'actionOpen_Media_Player')
         self.actionOpenMusicPlayer.setStatusTip('Open Media Player.')
         self.actionOpenMusicPlayer.triggered.connect(partial(self.open_mediaplayer_window, ''))
 
@@ -482,6 +482,7 @@ class mainwindowUI(QMainWindow):
                 for i, note in enumerate(genres_json[0]['Note Types'][0]):
                     if note_type_states[i] == 'True': all_available_note_types.append(note_types[note])
             while True:
+                self.setCursor(Qt.BusyCursor)
                 if not len(all_available_notes) > 0: return
                 else:
                     if algorithmName == 'Random':
@@ -522,6 +523,7 @@ class mainwindowUI(QMainWindow):
                             if i == len(all_available_step_notes): step_keys, step_note_keys = self.getStepNumberList()
                     self.updateProgressBar(progressBar, final_song.duration_seconds/seconds*100)
                     if final_song.duration_seconds >= seconds:
+                        self.unsetCursor()
                         final_name = (f'{str(algorithmName)} {str(self.genresComboBox.currentText())} {str(final_song.duration_seconds)}{str()}.mp3')
                         labelStatus.setText('Status: Finished!')
                         buttonName.setText(buttonName.text() + final_name)
@@ -580,10 +582,9 @@ class mainwindowUI(QMainWindow):
         self.settingsUI.show()
     def open_mediaplayer_window(self, name):
         file_path = os.path.dirname(os.path.abspath(__file__))
-        self.Player = Player([file_path + '/' + compile_folder + name])
+        self.Player = Player(file_path + '/' + compile_folder + name)
         self.Player.setStyleSheet(open("style.qss", "r").read())
         self.Player.show()
-
 class settingsUI(QWidget):
     def __init__(self):
         super().__init__()
@@ -661,16 +662,13 @@ class PlaylistModel(QAbstractItemModel):
     def changeItems(self, start, end):
         self.dataChanged.emit(self.index(start, 0),
                 self.index(end, self.ColumnCount))
-        
+
 class PlayerControls(QWidget):
     play = pyqtSignal()
     pause = pyqtSignal()
     stop = pyqtSignal()
-    next = pyqtSignal()
-    previous = pyqtSignal()
     changeVolume = pyqtSignal(int)
     changeMuting = pyqtSignal(bool)
-    changeRate = pyqtSignal(float)
  
     def __init__(self, parent=None):
         super(PlayerControls, self).__init__(parent)
@@ -684,15 +682,7 @@ class PlayerControls(QWidget):
         self.stopButton = QToolButton(clicked=self.stop)
         self.stopButton.setIcon(self.style().standardIcon(QStyle.SP_MediaStop))
         self.stopButton.setEnabled(False)
- 
-        self.nextButton = QToolButton(clicked=self.next)
-        self.nextButton.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaSkipForward))
- 
-        self.previousButton = QToolButton(clicked=self.previous)
-        self.previousButton.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaSkipBackward))
- 
+        
         self.muteButton = QToolButton(clicked=self.muteClicked)
         self.muteButton.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaVolume))
@@ -700,22 +690,13 @@ class PlayerControls(QWidget):
         self.volumeSlider = QSlider(Qt.Horizontal,
                 sliderMoved=self.changeVolume)
         self.volumeSlider.setRange(0, 100)
- 
-        self.rateBox = QComboBox(activated=self.updateRate)
-        self.rateBox.addItem("0.5x", 0.5)
-        self.rateBox.addItem("1.0x", 1.0)
-        self.rateBox.addItem("2.0x", 2.0)
-        self.rateBox.setCurrentIndex(1)
- 
+
         layout = QHBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.stopButton)
-        # layout.addWidget(self.previousButton)
         layout.addWidget(self.playButton)
-        # layout.addWidget(self.nextButton)
         layout.addWidget(self.muteButton)
         layout.addWidget(self.volumeSlider)
-        layout.addWidget(self.rateBox)
         self.setLayout(layout)
  
     def state(self):
@@ -724,111 +705,70 @@ class PlayerControls(QWidget):
     def setState(self,state):
         if state != self.playerState:
             self.playerState = state
- 
             if state == QMediaPlayer.StoppedState:
                 self.stopButton.setEnabled(False)
-                self.playButton.setIcon(
-                        self.style().standardIcon(QStyle.SP_MediaPlay))
+                self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
             elif state == QMediaPlayer.PlayingState:
                 self.stopButton.setEnabled(True)
-                self.playButton.setIcon(
-                        self.style().standardIcon(QStyle.SP_MediaPause))
+                self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
             elif state == QMediaPlayer.PausedState:
                 self.stopButton.setEnabled(True)
-                self.playButton.setIcon(
-                        self.style().standardIcon(QStyle.SP_MediaPlay))
- 
+                self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+
     def volume(self):
         return self.volumeSlider.value()
- 
+
     def setVolume(self, volume):
         self.volumeSlider.setValue(volume)
- 
+
     def isMuted(self):
         return self.playerMuted
- 
+
     def setMuted(self, muted):
         if muted != self.playerMuted:
             self.playerMuted = muted
- 
-            self.muteButton.setIcon(
-                    self.style().standardIcon(
-                            QStyle.SP_MediaVolumeMuted if muted else QStyle.SP_MediaVolume))
- 
+            self.muteButton.setIcon(self.style().standardIcon(QStyle.SP_MediaVolumeMuted if muted else QStyle.SP_MediaVolume))
+
     def playClicked(self):
-        if self.playerState in (QMediaPlayer.StoppedState, QMediaPlayer.PausedState):
-            self.play.emit()
-        elif self.playerState == QMediaPlayer.PlayingState:
-            self.pause.emit()
- 
+        if self.playerState in (QMediaPlayer.StoppedState, QMediaPlayer.PausedState): self.play.emit()
+        elif self.playerState == QMediaPlayer.PlayingState: self.pause.emit()
+
     def muteClicked(self):
         self.changeMuting.emit(not self.playerMuted)
- 
-    def playbackRate(self):
-        return self.rateBox.itemData(self.rateBox.currentIndex())
- 
-    def setPlaybackRate(self, rate):
-        for i in range(self.rateBox.count()):
-            if qFuzzyCompare(rate, self.rateBox.itemData(i)):
-                self.rateBox.setCurrentIndex(i)
-                return
- 
-        self.rateBox.addItem("%dx" % rate, rate)
-        self.rateBox.setCurrentIndex(self.rateBox.count() - 1)
- 
-    def updateRate(self):
-        self.changeRate.emit(self.playbackRate())
 
 class Player(QWidget):
-    fullScreenChanged = pyqtSignal(bool)
     def __init__(self, playlist, parent=None):
         super(Player, self).__init__(parent)
-        self.colorDialog = None
         self.trackInfo = ""
         self.statusInfo = ""
         self.duration = 0
- 
+
         self.player = QMediaPlayer()
-        self.playlist = QMediaPlaylist()
-        self.player.setPlaylist(self.playlist)
- 
+        # self.playlist = QMediaPlaylist()
+        # self.player.setPlaylist(self.playlist)
+
         self.player.durationChanged.connect(self.durationChanged)
         self.player.positionChanged.connect(self.positionChanged)
         self.player.metaDataChanged.connect(self.metaDataChanged)
-        self.playlist.currentIndexChanged.connect(self.playlistPositionChanged)
+        # self.playlist.currentIndexChanged.connect(self.playlistPositionChanged)
         self.player.mediaStatusChanged.connect(self.statusChanged)
         self.player.bufferStatusChanged.connect(self.bufferingProgress)
-        self.player.videoAvailableChanged.connect(self.videoAvailableChanged)
         self.player.error.connect(self.displayErrorMessage)
+
+        # self.playlistModel = PlaylistModel()
+        # self.playlistModel.setPlaylist(self.playlist)
  
-        # self.videoWidget = VideoWidget()
-        # self.player.setVideoOutput(self.videoWidget)
+        # self.playlistView = QListView()
+        # self.playlistView.setModel(self.playlistModel)
+        # self.playlistView.setCurrentIndex(self.playlistModel.index(self.playlist.currentIndex(), 0))
  
-        self.playlistModel = PlaylistModel()
-        self.playlistModel.setPlaylist(self.playlist)
- 
-        self.playlistView = QListView()
-        self.playlistView.setModel(self.playlistModel)
-        self.playlistView.setCurrentIndex(self.playlistModel.index(self.playlist.currentIndex(), 0))
- 
-        self.playlistView.activated.connect(self.jump)
+        # self.playlistView.activated.connect(self.jump)
  
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setRange(0, self.player.duration() / 1000)
  
         self.labelDuration = QLabel()
         self.slider.sliderMoved.connect(self.seek)
- 
-        # self.labelHistogram = QLabel()
-        # self.labelHistogram.setText("Histogram:")
-        # self.histogram = HistogramWidget()
-        # histogramLayout = QHBoxLayout()
-        # histogramLayout.addWidget(self.labelHistogram)
-        # histogramLayout.addWidget(self.histogram, 1)
- 
-        # self.probe = QVideoProbe()
-        # self.probe.videoFrameProbed.connect(self.histogram.processFrame)
-        # self.probe.setSource(self.player)
  
         openButton = QPushButton("Open", clicked=self.open)
  
@@ -840,27 +780,15 @@ class Player(QWidget):
         controls.play.connect(self.player.play)
         controls.pause.connect(self.player.pause)
         controls.stop.connect(self.player.stop)
-        # controls.next.connect(self.playlist.next)
-        # controls.previous.connect(self.previousClicked)
         controls.changeVolume.connect(self.player.setVolume)
         controls.changeMuting.connect(self.player.setMuted)
-        controls.changeRate.connect(self.player.setPlaybackRate)
-        # controls.stop.connect(self.videoWidget.update)
  
         self.player.stateChanged.connect(controls.setState)
         self.player.volumeChanged.connect(controls.setVolume)
         self.player.mutedChanged.connect(controls.setMuted)
  
-        # self.fullScreenButton = QPushButton("FullScreen")
-        # self.fullScreenButton.setCheckable(True)
- 
-        # self.colorButton = QPushButton("Color Options...")
-        # self.colorButton.setEnabled(False)
-        # self.colorButton.clicked.connect(self.showColorDialog)
- 
-        displayLayout = QHBoxLayout()
-        # displayLayout.addWidget(self.videoWidget, 2)
-        # displayLayout.addWidget(self.playlistView)
+        self.fullScreenButton = QPushButton("FullScreen")
+        self.fullScreenButton.setCheckable(True)
  
         controlLayout = QHBoxLayout()
         controlLayout.setContentsMargins(0, 0, 0, 0)
@@ -868,17 +796,13 @@ class Player(QWidget):
         controlLayout.addStretch(1)
         controlLayout.addWidget(controls)
         controlLayout.addStretch(1)
-        # controlLayout.addWidget(self.fullScreenButton)
-        # controlLayout.addWidget(self.colorButton)
  
         layout = QVBoxLayout()
-        layout.addLayout(displayLayout)
         hLayout = QHBoxLayout()
         hLayout.addWidget(self.slider)
         hLayout.addWidget(self.labelDuration)
         layout.addLayout(hLayout)
         layout.addLayout(controlLayout)
-        # layout.addLayout(histogramLayout)
  
         self.setLayout(layout)
  
@@ -888,150 +812,85 @@ class Player(QWidget):
                     "Please check the media service plugins are installed.")
  
             controls.setEnabled(False)
-            self.playlistView.setEnabled(False)
+            # self.playlistView.setEnabled(False)
             openButton.setEnabled(False)
-            self.colorButton.setEnabled(False)
-            self.fullScreenButton.setEnabled(False)
  
         self.metaDataChanged()
-        print(playlist)
         self.addToPlaylist(playlist)
  
     def open(self):
-        fileNames, _ = QFileDialog.getOpenFileNames(self, "Open Files")
+        fileNames, _ = QFileDialog.getOpenFileName(self, "Open Files")
         self.addToPlaylist(fileNames)
  
-    def addToPlaylist(self, fileNames):
-        for name in fileNames:
-            fileInfo = QFileInfo(name)
-            if fileInfo.exists():
-                url = QUrl.fromLocalFile(fileInfo.absoluteFilePath())
-                if fileInfo.suffix().lower() == 'm3u':
-                    self.playlist.load(url)
-                else:
-                    self.playlist.addMedia(QMediaContent(url))
-            else:
-                url = QUrl(name)
-                if url.isValid():
-                    self.playlist.addMedia(QMediaContent(url))
- 
+    def addToPlaylist(self, fileName):
+        url = QUrl(fileName)
+        # if url.isValid(): self.playlist.addMedia(QMediaContent(url))
+        self.player.play()
     def durationChanged(self, duration):
         duration /= 1000
- 
         self.duration = duration
         self.slider.setMaximum(duration)
- 
+
     def positionChanged(self, progress):
         progress /= 1000
- 
-        if not self.slider.isSliderDown():
-            self.slider.setValue(progress)
- 
+        if not self.slider.isSliderDown(): self.slider.setValue(progress)
         self.updateDurationInfo(progress)
- 
+
     def metaDataChanged(self):
-        if self.player.isMetaDataAvailable():
-            self.setTrackInfo("%s - %s" % (
-                    self.player.metaData(QMediaMetaData.AlbumArtist),
-                    self.player.metaData(QMediaMetaData.Title)))
- 
-    def previousClicked(self):
-        # Go to the previous track if we are within the first 5 seconds of
-        # playback.  Otherwise, seek to the beginning.
-        if self.player.position() <= 5000:
-            self.playlist.previous()
-        else:
-            self.player.setPosition(0)
- 
-    def jump(self, index):
-        if index.isValid():
-            self.playlist.setCurrentIndex(index.row())
-            self.player.play()
- 
-    def playlistPositionChanged(self, position):
-        self.playlistView.setCurrentIndex(
-                self.playlistModel.index(position, 0))
- 
+        if self.player.isMetaDataAvailable(): self.setTrackInfo("%s" % (self.player.metaData(QMediaMetaData.Title)))
+        self.setWindowTitle(self.player.metaData(QMediaMetaData.Title))
+
+    # def jump(self, index):
+    #     if index.isValid():
+    #         self.playlist.setCurrentIndex(index.row())
+    #         self.player.play()
+
+    # def playlistPositionChanged(self, position):
+    #     self.playlistView.setCurrentIndex(self.playlistModel.index(position, 0))
+
     def seek(self, seconds):
         self.player.setPosition(seconds * 1000)
- 
+
     def statusChanged(self, status):
         self.handleCursor(status)
- 
-        if status == QMediaPlayer.LoadingMedia:
-            self.setStatusInfo("Loading...")
-        elif status == QMediaPlayer.StalledMedia:
-            self.setStatusInfo("Media Stalled")
-        elif status == QMediaPlayer.EndOfMedia:
-            QApplication.alert(self)
-        elif status == QMediaPlayer.InvalidMedia:
-            self.displayErrorMessage()
-        else:
-            self.setStatusInfo("")
- 
+        if status == QMediaPlayer.LoadingMedia: self.setStatusInfo("Loading...")
+        elif status == QMediaPlayer.StalledMedia:  self.setStatusInfo("Media Stalled")
+        elif status == QMediaPlayer.EndOfMedia: QApplication.alert(self)
+        elif status == QMediaPlayer.InvalidMedia: self.displayErrorMessage()
+        else: self.setStatusInfo("")
+
     def handleCursor(self, status):
-        if status in (QMediaPlayer.LoadingMedia, QMediaPlayer.BufferingMedia, QMediaPlayer.StalledMedia):
-            self.setCursor(Qt.BusyCursor)
-        else:
-            self.unsetCursor()
- 
+        if status in (QMediaPlayer.LoadingMedia, QMediaPlayer.BufferingMedia, QMediaPlayer.StalledMedia): self.setCursor(Qt.BusyCursor)
+        else: self.unsetCursor()
+
     def bufferingProgress(self, progress):
         self.setStatusInfo("Buffering %d%" % progress)
- 
-    def videoAvailableChanged(self, available):
-        if available:
-            self.fullScreenButton.clicked.connect(
-                    self.videoWidget.setFullScreen)
-            self.videoWidget.fullScreenChanged.connect(
-                    self.fullScreenButton.setChecked)
- 
-            if self.fullScreenButton.isChecked():
-                self.videoWidget.setFullScreen(True)
-        else:
-            self.fullScreenButton.clicked.disconnect(
-                    self.videoWidget.setFullScreen)
-            self.videoWidget.fullScreenChanged.disconnect(
-                    self.fullScreenButton.setChecked)
- 
-            self.videoWidget.setFullScreen(False)
- 
-        self.colorButton.setEnabled(available)
- 
+
     def setTrackInfo(self, info):
         self.trackInfo = info
- 
-        if self.statusInfo != "":
-            self.setWindowTitle("%s | %s" % (self.trackInfo, self.statusInfo))
-        else:
-            self.setWindowTitle(self.trackInfo)
- 
+
+        if self.statusInfo != "": self.setWindowTitle("%s | %s" % (self.trackInfo, self.statusInfo))
+        else: self.setWindowTitle(self.trackInfo)
+
     def setStatusInfo(self, info):
         self.statusInfo = info
- 
-        if self.statusInfo != "":
-            self.setWindowTitle("%s | %s" % (self.trackInfo, self.statusInfo))
-        else:
-            self.setWindowTitle(self.trackInfo)
- 
+        if self.statusInfo != "": self.setWindowTitle("%s | %s" % (self.trackInfo, self.statusInfo))
+        else: self.setWindowTitle(self.trackInfo)
+
     def displayErrorMessage(self):
         self.setStatusInfo(self.player.errorString())
- 
+
     def updateDurationInfo(self, currentInfo):
         duration = self.duration
         if currentInfo or duration:
-            currentTime = QTime((currentInfo/3600)%60, (currentInfo/60)%60,
-                    currentInfo%60, (currentInfo*1000)%1000)
-            totalTime = QTime((duration/3600)%60, (duration/60)%60,
-                    duration%60, (duration*1000)%1000);
- 
+            currentTime = QTime((currentInfo/3600)%60, (currentInfo/60)%60, currentInfo%60, (currentInfo*1000)%1000)
+            totalTime = QTime((duration/3600)%60, (duration/60)%60, duration%60, (duration*1000)%1000);
+
             format = 'hh:mm:ss' if duration > 3600 else 'mm:ss'
             tStr = currentTime.toString(format) + " / " + totalTime.toString(format)
-        else:
-            tStr = ""
- 
+        else: tStr = ""
         self.labelDuration.setText(tStr)
- 
- 
+
 def load_config_file(*args):
     for i, j in enumerate(args):
         j.clear()
