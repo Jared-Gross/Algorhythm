36193aaa9752eeba6a259c95a31d5de49614fe15
JareBear <jaredgrozz@gmail.com>
2021-03-11 15:04:06 -0600

    stuff

--- main.cpp
+++ main.cpp
@@ -6,6 +6,18 @@
 #include <sstream>
 #include <vector>
 #include <time.h>
+#include "process.h"
+#include <iterator>
+#include <algorithm>
+// if were on windows
+#include <windows.h>
+#include <limits.h>
+#include <direct.h>
+
+// if were on linux
+#include <unistd.h>
+
+#include <fstream>
 
 using namespace std;
 /*
@@ -20,8 +32,52 @@ make install
 
 sudo apt install sox
 sudo apt-get install libsox-fmt-mp3
+
+WINDOWS
+install sox
+https://www.videohelp.com/software?d=sox-14.4.0-libmad-libmp3lame.zip
+copy and replace the contents to where ever you install sox to.
+
 */
+
 const string MUSIC_DIRECTORY = "./Piano Samples/";
+
+string CWD = "";
+
+string platform()
+{
+#ifdef _WIN32
+    return "Windows";
+#elif _WIN64
+    return "Windows";
+#elif __APPLE__ || __MACH__
+    return "Mac";
+#elif __linux__
+    return "Linux";
+#elif __FreeBSD__
+    return "FreeBSD";
+#elif __unix || __unix__
+    return "Unix";
+#else
+    return "Other";
+#endif
+}
+// template <size_t N>
+// void splitString(string (&arr)[N], string str)
+// {
+//     int n = 0;
+//     istringstream iss(str);
+//     for (auto it = istream_iterator<string>(iss); it != istream_iterator<string>() && n < N; ++it, ++n)
+//         arr[n] = *it;
+// }
+string getCurrentDir()
+{
+    char buff[MAX_PATH];
+    GetModuleFileName(NULL, buff, MAX_PATH);
+    string::size_type position = string(buff).find_last_of("\\/");
+    return string(buff).substr(0, position);
+}
+
 vector<string> list_dir(const char *path)
 {
     struct dirent *entry;
@@ -40,17 +96,27 @@ vector<string> list_dir(const char *path)
     {
         if (file_name != "." && file_name != "..")
         {
-            list_directory2.push_back(path + file_name);
+            replace(CWD.begin(), CWD.end(), '\\', '/'); // replace all 'x' to 'y'
+            list_directory2.push_back(CWD + "/Piano Samples/" + file_name);
         }
     }
     return list_directory2;
 }
 
-void combine_audio_files(vector<string> files, string output_filename)
+void combine_audio_files(vector<string> notes, vector<double> note_types, string output_filename)
 {
+    string CWD = getCurrentDir();
+    _mkdir((CWD + "/Process").c_str());
     string file_names_string = "";
-    for (string &file_name : files)
+    int index = 0;
+    for (string &file_name : notes)
+    {
+        string s_index = to_string(index);
+        string trim_ammount = to_string(note_types[index]);
+        system(("sox \"" + file_name + "\" \"" + CWD + "/Process/" + s_index + ".mp3\" reverse trim " + trim_ammount + " reverse").c_str());
         file_names_string.append(" \"" + file_name + "\" ");
+        index++;
+    }
     // NEED LENGTH OF AUDIO FILE
     // NEED TO SHRING THE AUDIO FILE LENGTH BY 1,2,4,8,16,32
     // SAVE THOSE SHRUNKEN AUDIO FILES TO A TEMPERARY Directory
@@ -58,10 +124,8 @@ void combine_audio_files(vector<string> files, string output_filename)
     // COMBINE ALL SHORTEND FILES INTO ONE
     // sox tracks\5_7.mp3 ntracks\05_7.mp3 reverse trim 0.195 reverse
 
-    string command_string = "cat" + file_names_string + "| mp3cat - - > \"" + output_filename + "\"";
-    // char *command = &(command_string[0]);
-    popen(command_string.c_str(), "r");
-    // popen(command, "r");
+    // string command_string = "cat" + file_names_string + "| mp3cat - - > \"" + output_filename + "\"";
+    // popen(command_string.c_str(), "r");
 }
 string get_audio_file_length(string command)
 {
@@ -83,11 +147,21 @@ string get_audio_file_length(string command)
     pclose(pipe);
     return result;
 }
+
 int main()
 {
+    if (platform() == "Windows")
+    {
+        // #include <windows.h>
+        // #include <limits.h>
+        CWD = getCurrentDir();
+    }
+    else if (platform() == "Linux")
+    {
+        // #include <unistd.h>
+        // CWD = get_current_dir_name();
+    }
     vector<string> list_directory = list_dir("./Piano Samples/");
-    for (string &path : list_directory)
-        cout << path << endl;
     vector<string> files_to_compile = {
         list_directory[1],
         list_directory[15],
@@ -99,9 +173,27 @@ int main()
         list_directory[1],
         list_directory[15],
     };
+    vector<double> note_types_value = {
+        8,
+        16,
+        32,
+        4,
+        2,
+        1,
+        4,
+        16,
+        32};
+    vector<double> trim_note_audio_values;
     int index = 0;
-    // combine_audio_files(files_to_compile, "yteaa.mp3");
-    string ls = get_audio_file_length("sox \"./Piano Samples/A1.mp3\" -n stat");
-    cout << ls;
+    for (string &file_name : files_to_compile)
+    {
+        string length = get_audio_file_length("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"" + file_name + "\"");
+        double audio_length = ::atof(length.c_str());
+        trim_note_audio_values.push_back(audio_length / note_types_value[index]);
+        index++;
+    }
+    for (double &new_trim : trim_note_audio_values)
+        cout << new_trim << endl;
+    combine_audio_files(files_to_compile, trim_note_audio_values, "yteaa.mp3");
     return 0;
 }
